668f34e2d08379dc5b9a23e38e504b67e752bf98
andr3s010607 <geovannyandres06@gmail.com>
2026-06-26 19:26:13 -0500

    Add Student, Teacher, Subject entities
    Introduce JPA entities for the academic domain and register a ModelMapper bean. Adds Student and Teacher (both extending User), and Subject entities with relationships: Teacher -> OneToMany subjects, Subject -> ManyToOne teacher and ManyToMany students, Student -> ManyToMany subjects (join table 'student_subject'). Also registers a ModelMapper @Bean in AcademicManagementApplication to support DTO mappings.

--- Backend/academic-management/src/main/java/io/github/andr3s010607/academic_management/AcademicManagementApplication.java
+++ Backend/academic-management/src/main/java/io/github/andr3s010607/academic_management/AcademicManagementApplication.java
@@ -1,7 +1,9 @@
 package io.github.andr3s010607.academic_management;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AcademicManagementApplication {
@@ -9,5 +11,11 @@ public class AcademicManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AcademicManagementApplication.class, args);
 	}
-
+	
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+		
+	}
+	
 }
