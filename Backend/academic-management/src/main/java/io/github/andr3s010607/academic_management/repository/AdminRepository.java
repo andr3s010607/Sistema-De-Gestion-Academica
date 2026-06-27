3626c9bfa292b3401e4a0177380f2d1221b1a256
andr3s010607 <geovannyandres06@gmail.com>
2026-06-26 20:44:34 -0500

    Add JPA repositories for entities
    Introduce Spring Data JPA repository interfaces for Admin, Student, Teacher, and Subject. Each extends JpaRepository to provide standard CRUD; Student and Teacher include findByUsername/deleteByUsername, and Subject includes findByName/deleteByName and findBySubjectCode/deleteBySubjectCode to support common lookups and deletions.

---
+++ Backend/academic-management/src/main/java/io/github/andr3s010607/academic_management/repository/AdminRepository.java
@@ -0,0 +1,9 @@
+package io.github.andr3s010607.academic_management.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.andr3s010607.academic_management.entity.Admin;
+
+public interface AdminRepository extends JpaRepository<Admin, Long>{
+
+}
---
+++ Backend/academic-management/src/main/java/io/github/andr3s010607/academic_management/repository/StudentRepository.java
@@ -0,0 +1,15 @@
+package io.github.andr3s010607.academic_management.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.andr3s010607.academic_management.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long>{
+	
+	public Optional<Student> findByUsername(String username);
+
+	public void deleteByUsername(String username); 
+	
+}
